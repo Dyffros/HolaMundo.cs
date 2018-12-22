6e7d4363b7d97ca0529f6b2096f5e3c9d2f068f5
Dyffros <46058692+Dyffros@users.noreply.github.com>
2018-12-22 22:53:03 +0100

    Add files via upload

---
+++ HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hola Mundo");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Pulsa cualquier tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
